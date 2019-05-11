2e42c5bc79c2c6af987c9f55bc791f0346b0199a
Vlad Dinulescu <vladd_47@yahoo.com>
2019-05-11 12:20:27 +0300

    Stream files directly to disk without storing them in memory. Add progress status. Move configuration file outside of the jar. Minor fixes.

--- src/main/java/ro/vdin/wifiphotodl/Downloader.java
+++ src/main/java/ro/vdin/wifiphotodl/Downloader.java
@@ -1,29 +1,30 @@
 package ro.vdin.wifiphotodl;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -31,23 +32,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Downloader {
     private static final Logger log = LoggerFactory.getLogger(Downloader.class);
 
-    @Autowired
+    private static final byte[] COPY_BUFFER = new byte[4 * 1024];
+
     private ObjectMapper objectMapper;
 
-    @Autowired
     private RestTemplate restTemplate;
-    
-    @Autowired
-    private AppConfig appConfig; 
+
+    private AppConfig appConfig;
+
+    public Downloader(ObjectMapper objectMapper, RestTemplate restTemplate, AppConfig appConfig) {
+        this.objectMapper = objectMapper;
+        this.restTemplate = restTemplate;
+        this.appConfig = appConfig;
+    }
 
     public void download() throws URISyntaxException, JsonParseException, JsonMappingException, IOException {
         log.info("Starting download");
 
         int lib = appConfig.getLib();
-
         int startIndex = appConfig.getStartIndex();
         int endIndex = appConfig.getEndIndex();
-
         int selectionSize = appConfig.getSelectionSize();
         File saveDir = new File(appConfig.getSaveDir());
 
@@ -64,7 +68,7 @@ public class Downloader {
 
         for (int i = startIndex; i < endIndex; i += selectionSize) {
             IntStream range = IntStream.range(i, i + selectionSize);
-            String selection = range.boxed().map(i2 -> String.valueOf(i2)).collect(Collectors.joining(","));
+            String selection = range.boxed().map(String::valueOf).collect(Collectors.joining(","));
 
             log.debug("selection=" + selection);
 
@@ -73,7 +77,7 @@ public class Downloader {
                 throw new RuntimeException("File " + filename + " exists");
             }
 
-            int fileBytes = downloadSingle(baseUrl, lib, selection, filename);
+            long fileBytes = downloadSingle(baseUrl, lib, selection, filename);
             if (isEmptyZip(fileBytes)) {
                 log.info("Empty zip downloaded, stopping download.");
                 break;
@@ -81,13 +85,13 @@ public class Downloader {
         }
     }
 
-    private boolean isEmptyZip(int fileBytes) {
+    private boolean isEmptyZip(long fileBytes) {
         return fileBytes == 49;
     }
 
-    private int downloadSingle(URI baseUrl, int lib, String selection, File filename) throws JsonParseException,
-                                                                                      JsonMappingException,
-                                                                                      IOException {
+    private long downloadSingle(URI baseUrl, int lib, String selection, File filename) throws JsonParseException,
+                                                                                       JsonMappingException,
+                                                                                       IOException {
         var selectionId = startDownload(baseUrl, lib, selection);
         log.info("Selection ID: " + selectionId);
 
@@ -95,15 +99,62 @@ public class Downloader {
         return saveSelection(baseUrl, selectionId, filename);
     }
 
-    private int saveSelection(URI baseUrl, String selectionId, File filename) throws IOException {
+    private long saveSelection(URI baseUrl, String selectionId, File filename) throws IOException {
         // curl http://192.168.0.100:15555/zipdownload/KRWBHRTKDANRGMIM/images.zip
+
+        log.info("Saving to: {}", filename);
 
         String url = String.format("%s/zipdownload/%s/images.zip", baseUrl, selectionId);
 
-        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
-        Files.write(Paths.get(filename.getAbsolutePath()), imageBytes);
-        log.info("Saved to: {}, {} bytes", filename, imageBytes.length);
-        return imageBytes.length;
+        try (OutputStream outputStream = new FileOutputStream(filename)) {
+            ResponseExtractor<?> responseCallback = responseExtractor -> {
+                log.trace("Callback called, headers: {}", responseExtractor.getHeaders());
+                try {
+                    long expectedFileSize = responseExtractor.getHeaders().getContentLength();
+
+                    Thread t = startProgressThread(filename, expectedFileSize);
+
+                    IOUtils.copyLarge(responseExtractor.getBody(), outputStream, COPY_BUFFER);
+
+                    endProgressThread(t);
+                } catch (Throwable t) {
+                    log.error("Error: {}", t.getMessage(), t);
+                }
+                return null;
+            };
+
+            restTemplate.execute(url, HttpMethod.GET, null, responseCallback);
+        }
+
+        long imageBytes = filename.length();
+
+        log.info("Saved to: {}, {} bytes", filename, imageBytes);
+        return imageBytes;
+    }
+
+    private Thread startProgressThread(File filename, long expectedFileSize) {
+        var thread = new Thread(() -> {
+            long fileSize = 0;
+            while (fileSize < expectedFileSize) {
+                fileSize = filename.length();
+                int percent = (int) (fileSize * 100 / expectedFileSize);
+                log.debug("Downloaded {} MiB / {} MiB, {}% complete.",
+                          toMebiByte(fileSize),
+                          toMebiByte(expectedFileSize),
+                          percent);
+                sleep();
+            }
+        });
+        thread.start();
+        return thread;
+    }
+
+    private void endProgressThread(Thread t) throws InterruptedException {
+        t.join();
+    }
+
+    private long toMebiByte(long fileSize) {
+        return fileSize / 1024 / 1024;
     }
 
     private void pollUntilReadyToDownload(URI baseUrl, String selectionId) throws JsonParseException,
@@ -131,7 +182,7 @@ public class Downloader {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON_UTF8));
 
-        String compressUri = String.format("%s/compressprogress%s?%s", url, randomString(), selId);
+        String compressUri = String.format("%s/compressprogress?%s", url, selId);
 
         ResponseEntity<String> response = restTemplate.getForEntity(compressUri, String.class);
 
@@ -145,16 +196,12 @@ public class Downloader {
           }
           */
 
-        @SuppressWarnings("unchecked")
-        Map<String, Object> responseMap = objectMapper.readValue(response.getBody(), Map.class);
+        Map<String, Object> responseMap = objectMapper.readValue(response.getBody(),
+                                                                 new TypeReference<Map<String, Object>>() {
+                                                                 });
 
         Boolean readyForDownload = (Boolean) responseMap.get("readyForDownload");
         return readyForDownload;
-    }
-
-    private String randomString() {
-        // TODO: implement
-        return "";
     }
 
     private String startDownload(URI baseUrl, int lib, String selection) throws IOException,
@@ -164,22 +211,18 @@ public class Downloader {
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON_UTF8));
 
-        var formBody = new LinkedMultiValueMap<String, String>();
-        formBody.add("lib", String.valueOf(lib));
-        formBody.add("sel", selection);
+        String formBody = String.format("lib=%s&sel=%s", lib, selection);
 
-        var request = new HttpEntity<MultiValueMap<String, String>>(formBody, headers);
+        var request = new HttpEntity<String>(formBody, headers);
 
-        log.info("Request factory: " + restTemplate.getRequestFactory().getClass());
-
-        log.info("Resolving url");
+        //        log.info("Request factory: " + restTemplate.getRequestFactory().getClass());
 
         baseUrl = baseUrl.resolve("startcompressing");
 
-        log.info("Going at it!, url=" + baseUrl);
+        log.info("Starting to download photos!, baseUrl=" + baseUrl);
         ResponseEntity<String> response = restTemplate.postForEntity(baseUrl, request, String.class);
 
-        log.info("Response: " + response);
+        log.debug("Response: " + response);
 
         @SuppressWarnings("unchecked")
         Map<String, Object> readValue = objectMapper.readValue(response.getBody(), Map.class);
--- src/main/java/ro/vdin/wifiphotodl/WifiPhotoDownloadApplication.java
+++ src/main/java/ro/vdin/wifiphotodl/WifiPhotoDownloadApplication.java
@@ -1,14 +1,16 @@
 package ro.vdin.wifiphotodl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WifiPhotoDownloadApplication implements CommandLineRunner {
-    @Autowired
     private Downloader downloader;
+
+    public WifiPhotoDownloadApplication(Downloader downloader) {
+        this.downloader = downloader;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WifiPhotoDownloadApplication.class, args);
